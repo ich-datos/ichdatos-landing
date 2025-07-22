b07c196f65e4b327e78c7bee1c92672dc8099ad9
ich-datos <conecta@ichdatos.com.ar>
2025-07-22 00:05:04 -0300

    Modifica hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,8 +2,8 @@
 export default function Hero() {
   return (
     <section className="h-[90vh] flex flex-col justify-center items-center text-center px-5 bg-gray-950">
-      <h2 className="text-4xl md:text-3xl mb-4 text-gray-100">
-        TU PYME YA TIENE TODO... NOSOTROS LO CONECTAMOS.
+      <h2 className="text-4xl md:text-3xl mb-4 text-gray-100 font-bold">
+        TU PYME YA TIENE TODO. NOSOTROS LO CONECTAMOS.
       </h2>
       <p className="text-lg md:text-lg py-4 mb-35 max-w-1x2 text-gray-400">
         Ponemos a trabajar tus datos para que tomes decisiones asertivas.
